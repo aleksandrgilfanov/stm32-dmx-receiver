51fd591125aacb9d4758ff28da6e23d1156888dd
Aleksandr Gilfanov <gilfanov@numatech.ru>
2020-04-11 07:09:15 +0300

    adc: increase sampling time, to make adc interrupts less frequent

--- Src/main.c
+++ Src/main.c
@@ -213,7 +213,7 @@ static void MX_ADC1_Init(void)
   */
   sConfig.Channel = ADC_CHANNEL_6;
   sConfig.Rank = ADC_REGULAR_RANK_1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_28CYCLES_5;
+  sConfig.SamplingTime = ADC_SAMPLETIME_239CYCLES_5;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
